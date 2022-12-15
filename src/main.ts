3ce953c17f41ae68cd76c5ac1448e620ba37848c
primky <fakeprimky@gmail.com>
2022-12-15 16:33:47 +0300

    create: added routes to quiz and minor refactor with adding `dotenv` library

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+require('dotenv').config()
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3010);
+  await app.listen(PORT, ()=> console.log(`server started on port ${PORT}`));
 }
 bootstrap();
