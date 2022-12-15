bac8580fdf235f0d2d213ce77e13cbf0cbcc2d0b
primky <fakeprimky@gmail.com>
2022-12-15 16:33:47 +0300

    create: added routes to quiz and minor refactor with adding `dotenv` library

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from "@nestjs/common";
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/quiz')
+  getQuiz(): string {
+    return this.appService.getQuiz()
+  }
+
+  @Get('/quiz/:id')
+  getOneQuiz(@Param('id') id : string): string {
+    return this.appService.getOneQuiz(id)
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getQuiz(): string {
+    return 'All quiz'
+  }
+
+  getOneQuiz( id: string): string {
+    return `only one quiz with id = ${id}`;
   }
 }
