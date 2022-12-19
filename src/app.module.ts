7f8754d0e96e6bf3ffe940fc4f2ff339b6716bf8
YuryHouse <yury.house@yahoo.com>
2022-12-19 20:22:46 +0300

    .env file was created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuizModule } from "./quiz/quiz.module";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [QuizModule],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env'
+  }),
+    QuizModule],
   controllers: [AppController],
   providers: [AppService],
 })
