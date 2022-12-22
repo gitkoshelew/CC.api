86ddf1763d12af8624f4c491672a53f7b51b89ef
FARYD <faridkarmanov@gmail.com>
2022-12-22 12:42:11 +0300

    fixed(api): fixed errors with docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,13 +11,13 @@ import { SequelizeModule } from '@nestjs/sequelize';
     envFilePath: `.${process.env.NODE_ENV}.env`
   }),  SequelizeModule.forRoot({
     dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '1234',
-    database: 'database',
-    models: [],
-    autoLoadModels: true,
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [],
+      autoLoadModels: true
   }),
   QuizModule],
   controllers: [AppController],
