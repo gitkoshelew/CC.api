48268b7fdc93bcbd61ee35b58c799c0c96844a84
FARYD <faridkarmanov@gmail.com>
2023-01-12 16:03:38 +0300

    fix

--- src/moderation/moderation.model.ts
+++ src/moderation/moderation.model.ts
@@ -1,13 +1,13 @@
 import { Column, DataType, HasOne, Model, Table } from 'sequelize-typescript';
 import { Question } from '../questions/questions.model';
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
 
 export enum ModerationStatus {
   review = 'review',
   change = 'change',
   block = 'block',
   rewrite = 'rewrite',
-  accept = 'accept'
+  accept = 'accept',
 }
 
 interface ModerationCreationAttrs {
@@ -15,22 +15,34 @@ interface ModerationCreationAttrs {
   status: ModerationStatus;
 }
 
-@Table({ tableName: 'moderations', createdAt: false, updatedAt: false})
+@Table({ tableName: 'moderations', createdAt: false, updatedAt: false })
 export class Moderation extends Model<Moderation, ModerationCreationAttrs> {
-
-  @ApiProperty({example: '1', description: 'uniq id / number'})
-  @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
+  @ApiProperty({ example: '1', description: 'uniq id / number' })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
   id: number;
 
-  @ApiProperty({example: 'Rewrite please some points that were doubt', description: 'comment what to do / string'})
+  @ApiProperty({
+    example: 'Rewrite please some points that were doubt',
+    description: 'comment what to do / string',
+  })
   @Column({ type: DataType.STRING, allowNull: false })
   comment: string;
 
-  @ApiProperty({example: 'rewrite', description: 'enum status (review/rewrite/change/block/accept)'})
-  @Column({ type: DataType.ENUM, values: ['review', 'change', 'block', 'rewrite', 'accept'] })
+  @ApiProperty({
+    example: 'rewrite',
+    description: 'enum status (review/rewrite/change/block/accept)',
+  })
+  @Column({
+    type: DataType.ENUM,
+    values: ['review', 'change', 'block', 'rewrite', 'accept'],
+  })
   status: ModerationStatus;
 
-  //@ApiProperty({example: '[questionId (2) ]', description: 'property to understand for what question relates this moderation field'})
   @HasOne(() => Question)
   question: Question;
 }
