c49354a9dfcc2b98e6e73a5e7847c34c05f66f82
Artur <82472015+benyaar@users.noreply.github.com>
2023-02-04 22:06:22 +0200

    feat: add permission decorator

---
+++ src/auth/decorators/permission.decorator.ts
@@ -0,0 +1,6 @@
+import { SetMetadata } from '@nestjs/common';
+
+export const PERMISSION_KEY = 'Permission';
+
+export const Permissions = (permissions: string) =>
+  SetMetadata(PERMISSION_KEY, permissions);
