c3c6bc42a9a15a51fe3c82511ee33c10ad5e5061
farid <farid@example.com>
2023-03-24 12:51:53 +0300

    feat(cc.api): were add new dto and logic

--- src/questions/dto/create-questionForQuiz.dto.ts
+++ src/questions/dto/create-questionForQuiz.dto.ts
@@ -1,45 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { DifficultyTypes, QuestionTypes } from '../questions.types';
+import { CreateQuestionDto } from './create-question.dto';
 
-export class CreateQuestionForQuizDto {
-  @ApiProperty({
-    example: 'NodeJS question #13',
-    description: 'required title / not uniq / string',
-  })
-  readonly title: string;
-  @ApiProperty({
-    example: {
-      options: ['answer1', 'answer2', 'answer3', 'answer4'],
-      correctAnswer: ['answer2'],
-    },
-    description: 'sample of question at JSON datatype/ required',
-  })
-  readonly content: JSON;
-  @ApiProperty({
-    example: 'single / multi',
-    description: 'how many of variables can been chosen for answer / required',
-  })
-  readonly type: QuestionTypes;
-  @ApiProperty({
-    example: 'light / medium / hard',
-    description: 'set your difficulty / required',
-  })
-  readonly difficulty: DifficultyTypes;
-  @ApiProperty({
-    example: 'this question can find your weakness at EventLoop',
-    description: 'piece of description for the question / required',
-  })
-  readonly description: string;
-  @ApiProperty({
-    example: 1,
-    description: 'topic of the question / required',
-  })
-  readonly topicId: number;
-  @ApiProperty({
-    example: 1000,
-    description: 'timer in seconds / required',
-  })
-  readonly timer: number;
+export class CreateQuestionForQuizDto extends CreateQuestionDto {
   @ApiProperty({
     example: 1,
     description: 'quiz of the question / required',
